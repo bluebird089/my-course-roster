49bf1dd89de2edc1bb4bf24246e372ae523afdef
bluebird089 <bluebird089@proton.me>
2023-09-16 13:50:11 +0600

    added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className='text-center font-bold py-5 text-3xl'>
+            <h1>Course Registration</h1>            
+        </div>
+    );
+};
+
+export default Header;
